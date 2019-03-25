712d552c2df7b7f0aadd33afeb2d5cb73bb8c74b
izeren <yushalnik@bk.ru>
2019-03-25 21:33:17 +0300

    Switch off old EventManager. It should be rewritten later or just completely removed

--- EventManager.cpp
+++ EventManager.cpp
@@ -8,32 +8,32 @@ void EventManager::RegisterSpikeEvent( Event event, Time time ) {
 }
 
 void EventManager::RunSimulation() {
-    for ( Time time = 0; time < simulationTime; time++ ) {
-        for ( Event event: eventsTimeline[time] ) {
-            eventCounter += 1;
-            auto synapses = event->outputSynapses;
-            if ( event->v > event->vMaxThresh ) {
-                if ( time + 1 < simulationTime ) {
-                    RegisterSpikeEvent( event, time + 1);
-                }
-            }
-            for ( int synapseId = 0; synapseId < synapses.size(); synapseId++ ) {
-                LifNeuron *next = synapses[synapseId].next;
-                if ( next->IsConsistent()) {
-                    validationTimeline[time].push_back(next);
-                }
-                next->UpdatePotential(time, synapses[synapseId].strength);
-            }
-        }
-        for ( ValidationCandidate candidate: validationTimeline[time] ) {
-            bool isActive = candidate->NormalizePotential(time);
-            if ( isActive ) {
-                if ( time + 1 < simulationTime ) {
-                    RegisterSpikeEvent( candidate, time + 1 );
-                }
-            }
-        }
-    }
+//    for ( Time time = 0; time < simulationTime; time++ ) {
+//        for ( Event event: eventsTimeline[time] ) {
+//            eventCounter += 1;
+//            auto synapses = event->GetOutputSynapses();
+//            if ( event->v > event->vMaxThresh ) {
+//                if ( time + 1 < simulationTime ) {
+//                    RegisterSpikeEvent( event, time + 1);
+//                }
+//            }
+//            for ( int synapseId = 0; synapseId < synapses.size(); synapseId++ ) {
+//                LifNeuron *next = synapses[synapseId].next;
+//                if ( next->IsConsistent()) {
+//                    validationTimeline[time].push_back(next);
+//                }
+//                next->UpdatePotential(time, synapses[synapseId].strength);
+//            }
+//        }
+//        for ( ValidationCandidate candidate: validationTimeline[time] ) {
+//            bool isActive = candidate->NormalizePotential(time);
+//            if ( isActive ) {
+//                if ( time + 1 < simulationTime ) {
+//                    RegisterSpikeEvent( candidate, time + 1 );
+//                }
+//            }
+//        }
+//    }
 }
 
 EventManager::EventManager( int simulationTime ) : simulationTime( simulationTime ) {
