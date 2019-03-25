edbc490c40e1673216400138dca12b61cfd75b58
izeren <yushalnik@bk.ru>
2019-03-25 21:33:46 +0300

    Add virtual destructor to IEventManager

--- IEventManager.h
+++ IEventManager.h
@@ -11,6 +11,8 @@ class IEventManager {
 public:
     IEventManager() = default;
 
+    virtual ~IEventManager() = default;
+
     virtual void RunSimulation( SPIKING_NN::Time time ) = 0;
 
     virtual void RegisterSample( const SPIKING_NN::Sample &sample, const SPIKING_NN::Layer &input) = 0;
