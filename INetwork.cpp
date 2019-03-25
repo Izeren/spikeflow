f70cff1ef160a623df1370de29fa34c8ab4e3dba
izeren <yushalnik@bk.ru>
2019-03-25 18:31:50 +0300

    Add basic forward/scoring/destruction logic to INetwork

--- INetwork.cpp
+++ INetwork.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "INetwork.h"
+#include "INeuron.h"
+#include "IEventManager.h"
+
+INetwork::~INetwork() {
+    for ( auto neuron: input ) {
+        delete neuron;
+    }
+    for ( auto neuron: hidden ) {
+        delete neuron;
+    }
+    for ( auto neuron: output ) {
+        delete neuron;
+    }
+}
+
+void INetwork::forward( const SPIKING_NN::Sample &sample, std::vector<float> &output, SPIKING_NN::Time time ) {
+    eventManager->RegisterSample( sample, input );
+    eventManager->RunSimulation( time );
+    output.resize( this->output.size());
+    for ( int neuronId = 0; neuronId < output.size(); ++neuronId ) {
+        output[neuronId] = this->output[neuronId]->GetOutput();
+    }
+}
+
+SPIKING_NN::Score INetwork::ScoreModel( SPIKING_NN::Dataset &data, SPIKING_NN::LossFunction lossFunction, bool onTest,
+                                        SPIKING_NN::Time time ) {
+    std::vector<SPIKING_NN::Output> predictions;
+    std::vector<SPIKING_NN::Sample> &samples = (onTest ? data.xTest : data.xTrain);
+    std::vector<SPIKING_NN::Target> &labels = (onTest ? data.yTest : data.yTrain);
+    for ( int sampleId = 0; sampleId < samples.size(); ++sampleId ) {
+        forward( samples[sampleId], predictions[sampleId], time );
+    }
+    return lossFunction( predictions, labels );
+}
--- INetwork.h
+++ INetwork.h
@@ -2,19 +2,36 @@
 // Created by izeren on 3/22/19.
 //
 #pragma once
+
 #include <vector>
+#include "SpikingGeneral.h"
 
 class INeuron;
+
 class ISynapse;
 
-typedef std::vector<INeuron *> Layer;
+class IEventManager;
 
 class INetwork {
+public:
+    virtual ~INetwork();
+
+    virtual void InitInput( size_t size ) = 0;
+
+    virtual void InitOutput( size_t size ) = 0;
+
+    virtual void InitHidden( size_t size ) = 0;
+
+    virtual void forward( const SPIKING_NN::Sample &sample, std::vector<float> &output, SPIKING_NN::Time time );
+
+    virtual SPIKING_NN::Score
+    ScoreModel( SPIKING_NN::Dataset &data, SPIKING_NN::LossFunction function, bool onTest, SPIKING_NN::Time time );
+
 
 protected:
 
-    Layer input;
-    Layer output;
+    SPIKING_NN::Layer input;
+    SPIKING_NN::Layer output;
 
     /**
      * It is important note that hidden neurons not binded strictly to layers
@@ -22,8 +39,8 @@ protected:
      * and store all the neurons subsequently
      * This assumption gives us a huge power of indeterminate architectures
      */
-    Layer hidden;
-    bool isPerceptron;
-    std::vector<size_t> hiddenLayersSizes;
+    SPIKING_NN::Layer hidden;
+
+    IEventManager *eventManager;
 
 };
