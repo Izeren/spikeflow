ee869de7de090415d9547ae2d4a7edf2be7f9de6
izeren <yushalnik@bk.ru>
2019-03-25 21:38:31 +0300

    Add class LifNetwork to implement basic graph architectures on LifNeuron with PreciseEventManager

--- LifNetwork.h
+++ LifNetwork.h
@@ -1,14 +1,27 @@
 //
 // Created by izeren on 3/25/19.
 //
+#pragma once
 
-#ifndef SPIKEPROP_LIFNETWORK_H
-#define SPIKEPROP_LIFNETWORK_H
+#include <map>
+#include "INetwork.h"
+
+class LifNetwork : public INetwork {
+public:
+    LifNetwork( size_t inputSize = 0, size_t outputSize = 0 );
+
+    ~LifNetwork();
+
+    virtual void AddNeuron( size_t neuronId, SPIKING_NN::NEURON_TYPE neuronType ) override;
+
+    virtual void
+    AddLink( size_t preSynapticNeuronId, size_t postSynapticNeuronId, SPIKING_NN::Strength strength,
+             SPIKING_NN::Time delay ) override;
 
 
-class LifNetwork {
+protected:
+    std::map<size_t, INeuron *> neuronMap;
+    size_t size;
+
 
 };
-
-
-#endif //SPIKEPROP_LIFNETWORK_H
