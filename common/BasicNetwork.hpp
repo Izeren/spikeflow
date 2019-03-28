3304aef7f0e302f3cf144f4d59273443f23c85ed
izeren <yushalnik@bk.ru>
2019-03-28 16:24:20 +0300

    Move implementation of BasicNetwork outside of the class declaration

--- common/BasicNetwork.hpp
+++ common/BasicNetwork.hpp
@@ -10,41 +10,56 @@
 template<class Neuron, class Synapse, class EventManager>
 class BasicNetwork : public INetwork {
 public:
-    explicit BasicNetwork( size_t inputSize = 0, size_t outputSize = 0 ) {
-        size = 0;
-        for ( auto neuronId = 0; neuronId < inputSize; ++neuronId ) {
-            AddNeuron( size++, SPIKING_NN::NEURON_TYPE::INPUT );
-        }
-        for ( auto neuronId = 0; neuronId < outputSize; ++neuronId ) {
-            AddNeuron( size++, SPIKING_NN::NEURON_TYPE::OUTPUT );
-        }
-        eventManager = new EventManager();
-    }
+    explicit BasicNetwork( size_t inputSize = 0, size_t outputSize = 0 );
 
-    ~BasicNetwork() override {
-        for ( auto id_ptr: neuronMap ) {
-            delete id_ptr.second;
-        }
-        delete eventManager;
-    }
+    ~BasicNetwork() override;
 
-    void AddNeuron( size_t neuronId, SPIKING_NN::NEURON_TYPE neuronType ) {
-        INetwork::AddNeuron( new Neuron(), neuronId, neuronType );
-    }
+    void AddNeuron( size_t neuronId, SPIKING_NN::NEURON_TYPE neuronType );
 
     void AddLink( size_t preSynapticNeuronId, size_t postSynapticNeuronId, SPIKING_NN::Strength strength,
-                  SPIKING_NN::Time delay ) {
-        INetwork::AddLink( new Synapse(), preSynapticNeuronId, postSynapticNeuronId, strength, delay );
-    }
-
-
+                  SPIKING_NN::Time delay );
 protected:
     size_t size;
 
-    void Reset() override {
-        for ( auto id_ptr: neuronMap ) {
-            id_ptr.second->Reset();
-        }
-    }
+    void Reset() override;
 
 };
+
+template<class Neuron, class Synapse, class EventManager>
+BasicNetwork<Neuron, Synapse, EventManager>::BasicNetwork( size_t inputSize, size_t outputSize ) {
+    size = 0;
+    for ( auto neuronId = 0; neuronId < inputSize; ++neuronId ) {
+        AddNeuron( size++, SPIKING_NN::NEURON_TYPE::INPUT );
+    }
+    for ( auto neuronId = 0; neuronId < outputSize; ++neuronId ) {
+        AddNeuron( size++, SPIKING_NN::NEURON_TYPE::OUTPUT );
+    }
+    eventManager = new EventManager();
+}
+
+template<class Neuron, class Synapse, class EventManager>
+BasicNetwork<Neuron, Synapse, EventManager>::~BasicNetwork() {
+    for ( auto id_ptr: neuronMap ) {
+        delete id_ptr.second;
+    }
+    delete eventManager;
+}
+
+template<class Neuron, class Synapse, class EventManager>
+void BasicNetwork<Neuron, Synapse, EventManager>::AddNeuron( size_t neuronId, SPIKING_NN::NEURON_TYPE neuronType ) {
+    INetwork::AddNeuron( new Neuron(), neuronId, neuronType );
+}
+
+template<class Neuron, class Synapse, class EventManager>
+void BasicNetwork<Neuron, Synapse, EventManager>::AddLink( size_t preSynapticNeuronId, size_t postSynapticNeuronId,
+                                                           SPIKING_NN::Strength strength,
+                                                           SPIKING_NN::Time delay ) {
+    INetwork::AddLink( new Synapse(), preSynapticNeuronId, postSynapticNeuronId, strength, delay );
+}
+
+template<class Neuron, class Synapse, class EventManager>
+void BasicNetwork<Neuron, Synapse, EventManager>::Reset() {
+    for ( auto id_ptr : neuronMap ) {
+        id_ptr.second->Reset();
+    }
+}
